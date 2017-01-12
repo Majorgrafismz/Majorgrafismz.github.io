03196027de0770f132b0e1d48324886a91f5c7de
Laurent Abemango <abemango.laurent@gmail.com>
2017-01-12 20:56:04 +0100

    on est passé en php avec requires

---
+++ jadorigami_video.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<!--HEAD-->
+<?php require("rqrhead.html") ?>
+<body>
+  <!-- HEADER -->
+  <?php require ("rqrheader.html") ?>
+  <!-- FIN HEADER -->
+  <!-- MENU -->
+  <?php require ("rqrmenu.html") ?>
+  <!-- FIN MENU -->
+  <!-- mid page header-->
+  <header class="hicon">
+    <!--<p id="squarehicon_video"></p>-->
+    <!--<img id="logohicon_dsgn" src="images/logo_fel_icon30px.png" alt="jadorigami designs" title="jadorigami du design original"/>-->
+    <h3 id="hicon_video">VIDEOS</h3>
+  </header>
+  <!-- MAIN de la page -->
+  <main id="vid">
+
+    <div id="viddiv">
+      <article>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Zn3RX9N3fQk" frameborder="0" allowfullscreen></iframe>
+      </article>
+      <article>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/zbXG4XugaYM" frameborder="0" allowfullscreen></iframe>
+      </article>
+      <article>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/h9db0hMNb4g" frameborder="0" allowfullscreen></iframe>
+      </article>
+      <article>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/a3uw7FCOJaw" frameborder="0" allowfullscreen></iframe>
+      </article>
+      <article>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/zcer96e-DIU" frameborder="0" allowfullscreen></iframe>
+      </article>
+      <article>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/PGOjQEja-9U" frameborder="0" allowfullscreen></iframe>
+      </article>
+    </div>
+  </main>
+  <!--FOOTER-->
+  <?php require ("rqrfooter.html") ?>
+</body>
+</html>
